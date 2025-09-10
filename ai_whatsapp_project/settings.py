0a72a7ca6205edfbb7c976a3af7a2bc6a8056e76
AbdulrahmanSamir01 <s.abdulrahman.a.samir.2022@gmail.com>
2025-09-10 22:29:25 +0300

    add whitenoise

--- ai_whatsapp_project/settings.py
+++ ai_whatsapp_project/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # ضيفها هنا بعد SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,6 +150,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
